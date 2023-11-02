5c142079135216f3bbec72b1c2afd6d47d99c140
David R_ <36084356+infodavide@users.noreply.github.com>
2023-11-02 15:20:16 +0100

    Fix on FileCache

--- id-commons-io-utils/src/test/java/org/infodavid/commons/io/FileCacheTest.java
+++ id-commons-io-utils/src/test/java/org/infodavid/commons/io/FileCacheTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.InputStream;
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -66,6 +65,48 @@ class FileCacheTest extends TestCase {
      */
     @Test
     void testGetDataUsingFile() throws Exception {
+        final ProcessingDuration<byte[]> counter = new ProcessingDuration<>(() -> cache.getData(resource.toFile()));
+        final byte[] result1 = counter.run();
+        final long duration1 = counter.getDuration();
+
+        System.out.println("duration1: " + duration1); // NOSONAR
+
+        assertNotNull(result1, "Result is null");
+        assertEquals(resourceLength, result1.length, "Result length is wrong");
+        assertEquals(1, cache.getSize(), "Cache size is wrong");
+
+        final byte[] result2 = counter.run();
+        final long duration2 = counter.getDuration();
+
+        System.out.println("duration2: " + duration2); // NOSONAR
+
+        assertNotNull(result2, "Result is null");
+        assertEquals(result1.length, result2.length, "Result length is wrong");
+        assertArrayEquals(result1, result2, "Wrong data");
+        assertEquals(1, cache.getSize(), "Cache size is wrong");
+
+        final String text = String.valueOf(System.currentTimeMillis());
+
+        FileUtils.write(resource.toFile(), text, StandardCharsets.UTF_8);
+        sleep(200);
+
+        final byte[] result3 = counter.run();
+        final long duration3 = counter.getDuration();
+
+        System.out.println("duration3: " + duration3); // NOSONAR
+
+        assertNotNull(result3, "Result is null");
+        assertEquals(text.length(), result3.length, "Result length is wrong");
+        assertArrayEquals(text.getBytes(StandardCharsets.UTF_8), result3, "Wrong data");
+        assertEquals(1, cache.getSize(), "Cache size is wrong");
+    }
+
+    /**
+     * Test get data using URI.
+     * @throws Exception the exception
+     */
+    @Test
+    void testGetDataUsingPath() throws Exception {
         final ProcessingDuration<byte[]> counter = new ProcessingDuration<>(() -> cache.getData(resource));
         final byte[] result1 = counter.run();
         final long duration1 = counter.getDuration();
@@ -89,90 +130,7 @@ class FileCacheTest extends TestCase {
         final String text = String.valueOf(System.currentTimeMillis());
 
         FileUtils.write(resource.toFile(), text, StandardCharsets.UTF_8);
-
-        final byte[] result3 = counter.run();
-        final long duration3 = counter.getDuration();
-
-        System.out.println("duration3: " + duration3); // NOSONAR
-
-        assertNotNull(result3, "Result is null");
-        assertEquals(text.length(), result3.length, "Result length is wrong");
-        assertArrayEquals(text.getBytes(StandardCharsets.UTF_8), result3, "Wrong data");
-        assertEquals(1, cache.getSize(), "Cache size is wrong");
-    }
-
-    /**
-     * Test get data using URI.
-     * @throws Exception the exception
-     */
-    @Test
-    void testGetDataUsingUri() throws Exception {
-        final URI uri = resource.toUri();
-        final ProcessingDuration<byte[]> counter = new ProcessingDuration<>(() -> cache.getData(uri));
-        final byte[] result1 = counter.run();
-        final long duration1 = counter.getDuration();
-
-        System.out.println("duration1: " + duration1); // NOSONAR
-
-        assertNotNull(result1, "Result is null");
-        assertEquals(resourceLength, result1.length, "Result length is wrong");
-        assertEquals(1, cache.getSize(), "Cache size is wrong");
-
-        final byte[] result2 = counter.run();
-        final long duration2 = counter.getDuration();
-
-        System.out.println("duration2: " + duration2); // NOSONAR
-
-        assertNotNull(result2, "Result is null");
-        assertEquals(result1.length, result2.length, "Result length is wrong");
-        assertArrayEquals(result1, result2, "Wrong data");
-        assertEquals(1, cache.getSize(), "Cache size is wrong");
-
-        final String text = String.valueOf(System.currentTimeMillis());
-
-        FileUtils.write(resource.toFile(), text, StandardCharsets.UTF_8);
-
-        final byte[] result3 = counter.run();
-        final long duration3 = counter.getDuration();
-
-        System.out.println("duration3: " + duration3); // NOSONAR
-
-        assertNotNull(result3, "Result is null");
-        assertEquals(text.length(), result3.length, "Result length is wrong");
-        assertArrayEquals(text.getBytes(StandardCharsets.UTF_8), result3, "Wrong data");
-        assertEquals(1, cache.getSize(), "Cache size is wrong");
-    }
-
-    /**
-     * Test get data using URL.
-     * @throws Exception the exception
-     */
-    @Test
-    void testGetDataUsingUrl() throws Exception {
-        final URI uri = resource.toUri();
-        final ProcessingDuration<byte[]> counter = new ProcessingDuration<>(() -> cache.getData(uri.toURL()));
-        final byte[] result1 = counter.run();
-        final long duration1 = counter.getDuration();
-
-        System.out.println("duration1: " + duration1); // NOSONAR
-
-        assertNotNull(result1, "Result is null");
-        assertEquals(resourceLength, result1.length, "Result length is wrong");
-        assertEquals(1, cache.getSize(), "Cache size is wrong");
-
-        final byte[] result2 = counter.run();
-        final long duration2 = counter.getDuration();
-
-        System.out.println("duration2: " + duration2); // NOSONAR
-
-        assertNotNull(result2, "Result is null");
-        assertEquals(result1.length, result2.length, "Result length is wrong");
-        assertArrayEquals(result1, result2, "Wrong data");
-        assertEquals(1, cache.getSize(), "Cache size is wrong");
-
-        final String text = String.valueOf(System.currentTimeMillis());
-
-        FileUtils.write(resource.toFile(), text, StandardCharsets.UTF_8);
+        sleep(200);
 
         final byte[] result3 = counter.run();
         final long duration3 = counter.getDuration();
